07c45fbc3e4d8ea82c2443a2da35826dd5424e03
Mathew <mateusz.lu123@gmail.com>
2024-12-11 18:21:51 +0100

    fixed sztandars

--- GameClasses/Banner/BannerHelper.cs
+++ GameClasses/Banner/BannerHelper.cs
@@ -7,15 +7,15 @@ namespace BoardGameBackend.Helpers
         public static int BannerRewardTileAction(PlayerInGame player, int bannerType){
             var goldAdded = 0;
 
-            if(player.PlayerMercenaryManager.Mercenaries.Any(mercenary => mercenary.Faction.Id == bannerType)){
+            if(player.PlayerMercenaryManager.Mercenaries.Any(mercenary => mercenary.Faction?.Id == bannerType)){
                 goldAdded =+ 1;
             }
 
-            if(player.PlayerRolayCardManager.RolayCards.Any(royalCard => royalCard.Faction.Id == bannerType)){
+            if(player.PlayerRolayCardManager.RolayCards.Any(royalCard => royalCard.Faction?.Id == bannerType)){
                 goldAdded =+ 1;
             }
 
-            if(player.PlayerHeroCardManager.HeroCardsLeft.Any(card => card .Faction.Id == bannerType) || player.PlayerHeroCardManager.HeroCardsRight.Any(card => card .Faction.Id == bannerType)){
+            if(player.PlayerHeroCardManager.HeroCardsLeft.Any(card => card.Faction?.Id == bannerType) || player.PlayerHeroCardManager.HeroCardsRight.Any(card => card.Faction?.Id == bannerType)){
                 goldAdded =+ 1;
             }
 
